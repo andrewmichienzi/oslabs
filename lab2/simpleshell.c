d1be9f2d12e3b881f2cdc98551529f5a87934cb4
michiena <michiena@mail.gvsu.edu>
2016-09-10 16:46:05 -0400

    cleaned up code

--- lab2/simpleshell.c
+++ lab2/simpleshell.c
@@ -16,46 +16,39 @@ int main()
   		char *datain;
   		datain = malloc(SIZE);
 
-  		printf("\n\n");
+  		printf("\n");
   		printf(">>");
   		fgets(datain, SIZE, stdin);
 		if(!strcmp("\n", datain)){
-			printf("test");
 			free(datain);
 			continue;
 		}
+		//Remove new line at end from fgets
 		datain = strtok(datain, "\n");
   		//Quit if correct input
  		if(!strcmp(quit, datain)){
     			free(datain);
     			exit(0);
   		}
-  		/*for(i = 0; i < ; i++){
-    			command[i] = strsep(&datain, " ");
-  		}*/
 	  	char* command[50];	
   		int i = 0;
   		command[i] = strtok(datain, " ");
-		printf("command[%d] = %s", i, command[i]);
 		do{
 			i++;
 			command[i] = strtok(NULL, " ");
-			printf("\ncommand[%d] = %s", i, command[i]);
 		}while(command[i]);
-		command[i] = '\0';	
-  		pid_t pid, child;
+		//command[i] = '\0';	
+  		pid_t pid;
   		int status;
 
   		if((pid=fork())<0){
     			perror("fork failure");
     			free(datain);
+			free(quit);
     			exit(1);
   		}
-		printf("\npid = %d", (int)pid);		
-
   		if(pid==0){
     			//child
-    			printf("\nChild\n");
     			execvp(command[0], &command[0]);
       			perror("\nexec failedyooooooo");
       			free(datain);
@@ -63,14 +56,8 @@ int main()
   		}
   		else{
     			//Parent
-    			printf("\nParent\n");
-    			child = wait(&status);
-			printf("\nchild = %d", (int) child);
+    			wait(&status);
  		}
-  		printf("command 0 = %s",command[0]);
-  		printf("\n");
-  		printf("command 1 = %s",command[1]);
-  		printf("command 2 = %s", command[2]);
 		free(datain);
 	}
 	free(quit);
