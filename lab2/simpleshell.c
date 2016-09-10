6b4fbc27797393108f5858185e644852d1a39022
michiena <michiena@mail.gvsu.edu>
2016-09-10 16:23:11 -0400

    Have exec working

--- lab2/simpleshell.c
+++ lab2/simpleshell.c
@@ -2,48 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define SIZE 50
 int main()
 {
-  char *datain;
-  datain = malloc(SIZE);
+  	char *quit;
+  	//fgets adds a new line to the end of a string
+	quit = "quit";
+ 	while(1){ 
+  		char *datain;
+  		datain = malloc(SIZE);
 
-  printf("\n\n");
-  printf(">>");
-  fgets(datain, SIZE, stdin);
-  printf("%s", datain);
+  		printf("\n\n");
+  		printf(">>");
+  		fgets(datain, SIZE, stdin);
+		if(!strcmp("\n", datain)){
+			printf("test");
+			free(datain);
+			continue;
+		}
+		datain = strtok(datain, "\n");
+  		//Quit if correct input
+ 		if(!strcmp(quit, datain)){
+    			free(datain);
+    			exit(0);
+  		}
+  		/*for(i = 0; i < ; i++){
+    			command[i] = strsep(&datain, " ");
+  		}*/
+	  	char* command[50];	
+  		int i = 0;
+  		command[i] = strtok(datain, " ");
+		printf("command[%d] = %s", i, command[i]);
+		do{
+			i++;
+			command[i] = strtok(NULL, " ");
+			printf("\ncommand[%d] = %s", i, command[i]);
+		}while(command[i]);
+		command[i] = '\0';	
+  		pid_t pid, child;
+  		int status;
 
-  int i;
-  char* command[50];
-  for(i = 0; i < 50; i++){
-    command[i] = strsep(&datain, " ");
-  }
-  
+  		if((pid=fork())<0){
+    			perror("fork failure");
+    			free(datain);
+    			exit(1);
+  		}
+		printf("\npid = %d", (int)pid);		
 
-  pid_t pid, child;
-  int status;
-
-  if((pid=fork())<0){
-    perror("fork failure");
-    exit(1);
-  }
-
-  if(pid==0){
-    //child
-    if(execvp(command[0], &command[0])<0){
-      perror("exec failed");
-      exit(1);
-    }
-  }
-
-  else{
-    //Parent
-    child = wait(&status);
-  }
-  printf("%s",command[0]);
-  printf("\n");
-  printf("%s",command[3]);
-  return (0);
+  		if(pid==0){
+    			//child
+    			printf("\nChild\n");
+    			execvp(command[0], &command[0]);
+      			perror("\nexec failedyooooooo");
+      			free(datain);
+      			exit(1);	
+  		}
+  		else{
+    			//Parent
+    			printf("\nParent\n");
+    			child = wait(&status);
+			printf("\nchild = %d", (int) child);
+ 		}
+  		printf("command 0 = %s",command[0]);
+  		printf("\n");
+  		printf("command 1 = %s",command[1]);
+  		printf("command 2 = %s", command[2]);
+		free(datain);
+	}
+	free(quit);
+  	return (0);
 }
 
